ba34ea45429aee8007385cae9f3bf79111e95a30
RoeyFuchs <fuchsroey@gmail.com>
2020-12-02 19:22:10 +0200

    done

--- ex2.py
+++ ex2.py
@@ -3,7 +3,9 @@ from collections import Counter
 
 from held_out import held_out
 from lidstone import lidstone
-from utils import voc_size
+from utils import sum_model_probs, voc_size
+
+DEBUG = False  # for part 5
 
 
 def is_header(text):
@@ -29,32 +31,38 @@ def float_range(start, end, step):
         current += step
 
 
+f = open(sys.argv[4], "w")
+print(
+    "#Students", "Yuval Mor", "Roey Fuchs", "205380173", "205415342", sep="\t", file=f
+)
 """ init - part 1 """
-print("Output1:", sys.argv[1], sep="\t")
-print("Output2:", sys.argv[2], sep="\t")
-print("Output3:", sys.argv[3], sep="\t")
-print("Output4:", sys.argv[4], sep="\t")
-print("Output5:", voc_size, sep="\t")
-print("Output6:", 1 / voc_size, sep="\t")  # uniform distribution
+print("#Output1", sys.argv[1], sep="\t", file=f)
+print("#Output2", sys.argv[2], sep="\t", file=f)
+print("#Output3", sys.argv[3], sep="\t", file=f)
+print("#Output4", sys.argv[4], sep="\t", file=f)
+print("#Output5", voc_size, sep="\t", file=f)
+print("#Output6", 1 / voc_size, sep="\t", file=f)  # uniform distribution
 
 """ Development set preprocessing - part 2 """
 content = parse_file(sys.argv[1])  # parse dev file
 number_of_words = len(content)
-print("Output7:", number_of_words, sep="\t")
+print("#Output7", number_of_words, sep="\t", file=f)
 
 """ Lidstone model training - part 3 """
 training, vald = (
     content[: round(0.9 * len(content))],
     content[round(0.9 * len(content)) :],
 )
-print("Output8:", len(vald), sep="\t")  # number of events in the validation set
-print("Output9:", len(training), sep="\t")  # number of event in the training set
+print("#Output8", len(vald), sep="\t", file=f)  # number of events in the validation set
+print(
+    "#Output9", len(training), sep="\t", file=f
+)  # number of event in the training set
 training_counter = Counter(training)  # count words in the training set
 print(
-    "Output10:", len(training_counter), sep="\t"
+    "#Output10", len(training_counter), sep="\t", file=f
 )  # different events in the trainign set
 print(
-    "Output11:", training_counter[sys.argv[3]], sep="\t"
+    "#Output11", training_counter[sys.argv[3]], sep="\t", file=f
 )  # number of times of input word
 
 
@@ -65,13 +73,22 @@ for lambda_val in float_range(0, 2, 0.01):
     lidstone_models[lambda_val] = {"model": model, "perplexity": perp}
 
 
-print("Output12:", lidstone_models[0]["model"].get_prob(sys.argv[3]), sep="\t")
-print("Output13:", lidstone_models[0]["model"].get_prob("unseen-word"), sep="\t")
-print("Output14:", lidstone_models[0.10]["model"].get_prob(sys.argv[3]), sep="\t")
-print("Output15:", lidstone_models[0.10]["model"].get_prob("unseen-word"), sep="\t")
-print("Output16:", lidstone_models[0.01]["perplexity"], sep="\t")
-print("Output17:", lidstone_models[0.10]["perplexity"], sep="\t")
-print("Output18:", lidstone_models[1.00]["perplexity"], sep="\t")
+print("#Output12", lidstone_models[0]["model"].get_prob(sys.argv[3]), sep="\t", file=f)
+print(
+    "#Output13", lidstone_models[0]["model"].get_prob("unseen-word"), sep="\t", file=f
+)
+print(
+    "#Output14", lidstone_models[0.10]["model"].get_prob(sys.argv[3]), sep="\t", file=f
+)
+print(
+    "#Output15",
+    lidstone_models[0.10]["model"].get_prob("unseen-word"),
+    sep="\t",
+    file=f,
+)
+print("#Output16", lidstone_models[0.01]["perplexity"], sep="\t", file=f)
+print("#Output17", lidstone_models[0.10]["perplexity"], sep="\t", file=f)
+print("#Output18", lidstone_models[1.00]["perplexity"], sep="\t", file=f)
 best_lambda = 0
 for lam_val in lidstone_models:
     if (
@@ -79,33 +96,45 @@ for lam_val in lidstone_models:
         < lidstone_models[best_lambda]["perplexity"]
     ):
         best_lambda = lam_val
-print("Output19:", best_lambda, sep="\t")
-print("Output20:", lidstone_models[best_lambda]["perplexity"], sep="\t")
+print("#Output19", best_lambda, sep="\t", file=f)
+print("#Output20", lidstone_models[best_lambda]["perplexity"], sep="\t", file=f)
 
 held = held_out(content)
-print("Output21:", len(held.train), sep="\t")
-print("Output22:", len(held.held), sep="\t")
-print("Output23:", held.get_prob(sys.argv[3]), sep="\t")
-print("Output24:", held.get_prob("unseen-word"), sep="\t")
-### TEST HERE!!!!!!!!!!!!!!!!!!!! ###
+print("#Output21", len(held.train), sep="\t", file=f)
+print("#Output22", len(held.held), sep="\t", file=f)
+print("#Output23", held.get_prob(sys.argv[3]), sep="\t", file=f)
+print("#Output24", held.get_prob("unseen-word"), sep="\t", file=f)
 
-
+""" debug code - part 5 """
+if DEBUG:
+    print(
+        "DEBUG LIDSTONE",
+        sum_model_probs(
+            lidstone_models[best_lambda]["model"],
+            lidstone_models[best_lambda]["model"].counter_words,
+        ),
+        file=f,
+    )
+    print("DEBUG HELD OUT", sum_model_probs(held, held.r), file=f)
+""" test - part 6 """
 content_test = parse_file(sys.argv[2])  # parse test file
-print("Output25:", len(content_test), sep="\t")
+print("#Output25", len(content_test), sep="\t", file=f)
 print(
-    "Output26:",
+    "#Output26",
     lidstone_models[best_lambda]["model"].get_perplexity(content_test),
     sep="\t",
+    file=f,
 )
-print("Output27:", held.get_perplexity(content_test), sep="\t")
+print("#Output27", held.get_perplexity(content_test), sep="\t", file=f)
 
 print(
-    "Output28:",
+    "#Output28",
     "H"
     if held.get_perplexity(content_test)
     < lidstone_models[best_lambda]["model"].get_perplexity(content_test)
     else "L",
     sep="\t",
+    file=f,
 )
 for i in range(10):
     print(
@@ -115,4 +144,6 @@ for i in range(10):
         round(held.Nr[i], 5),
         round(held.tr[i], 5),
         sep="\t",
+        file=f,
     )
+f.close()
