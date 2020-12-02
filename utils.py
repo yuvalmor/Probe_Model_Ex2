c8239606c85d38c2149450e97328c87d6ac96e0d
RoeyFuchs <fuchsroey@gmail.com>
2020-12-02 19:23:20 +0200

    code style minor change

--- utils.py
+++ utils.py
@@ -6,13 +6,13 @@ voc_size = 300000
 
 
 def perplexity(model, data):
-    probs = [model.get_prob(vald_word) for vald_word in data]
+    probs = [model.get_prob(word) for word in data]
     probs_log = [
         log2(word_prob) if word_prob > 0 else log2(float_info.epsilon)
         for word_prob in probs
     ]
     sum_probs = reduce(lambda a, b: a + b, probs_log)
-    power_val = -sum_probs / len(data)
+    power_val = (-1 * sum_probs) / len(probs_log)
     return 2 ** power_val
 
 
